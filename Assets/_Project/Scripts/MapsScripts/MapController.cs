8d03f29096cb1b76f64e563b41fd4867b04441fb
fromanint <fromaninteractive@gmail.com>
2022-12-06 23:50:33 -0600

    Working with UI Overlay

--- Assets/_Project/Scripts/MapsScripts/MapController.cs
+++ Assets/_Project/Scripts/MapsScripts/MapController.cs
@@ -1,15 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Mapbox.Unity.Map;
-using Mapbox.Utils;
-using ARLocation;
+using UnityEngine.SceneManagement;
 
-
-public class MapController : MonoBehaviour
+namespace ARLocation.MapboxRoutes.SampleProject
 {
-
-     public enum LineType
+    public class MapController : MonoBehaviour
+    {
+        public enum LineType
         {
             Route,
             NextTarget
@@ -74,11 +72,301 @@ public class MapController : MonoBehaviour
 
         private State s = new State();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+        private GUIStyle _textStyle;
+        GUIStyle textStyle()
+        {
+            if (_textStyle == null)
+            {
+                _textStyle = new GUIStyle(GUI.skin.label);
+                _textStyle.fontSize = 48;
+                _textStyle.fontStyle = FontStyle.Bold;
+            }
 
-    /
+            return _textStyle;
+        }
+
+        private GUIStyle _textFieldStyle;
+        GUIStyle textFieldStyle()
+        {
+            if (_textFieldStyle == null)
+            {
+                _textFieldStyle = new GUIStyle(GUI.skin.textField);
+                _textFieldStyle.fontSize = 48;
+            }
+            return _textFieldStyle;
+        }
+
+        private GUIStyle _errorLabelStyle;
+        GUIStyle errorLabelSytle()
+        {
+            if (_errorLabelStyle == null)
+            {
+                _errorLabelStyle = new GUIStyle(GUI.skin.label);
+                _errorLabelStyle.fontSize = 24;
+                _errorLabelStyle.fontStyle = FontStyle.Bold;
+                _errorLabelStyle.normal.textColor = Color.red;
+            }
+
+            return _errorLabelStyle;
+        }
+
+
+        private GUIStyle _buttonStyle;
+        GUIStyle buttonStyle()
+        {
+            if (_buttonStyle == null)
+            {
+                _buttonStyle = new GUIStyle(GUI.skin.button);
+                _buttonStyle.fontSize = 48;
+            }
+
+            return _buttonStyle;
+        }
+
+        void Awake()
+        {
+            // MapboxMapCamera.gameObject.SetActive(false);
+            // Map.SetCenterLatitudeLongitude()
+        }
+
+        void Start()
+        {
+            NextTargetPathRenderer.enabled = false;
+            RoutePathRenderer.enabled = false;
+            ARLocationProvider.Instance.OnEnabled.AddListener(onLocationEnabled);
+            Map.OnUpdated += OnMapRedrawn;
+        }
+
+        private void OnMapRedrawn()
+        {
+            // Debug.Log("OnMapRedrawn");
+            if (currentResponse != null)
+            {
+                buildMinimapRoute(currentResponse);
+            }
+        }
+
+        private void onLocationEnabled(Location location)
+        {
+            Map.SetCenterLatitudeLongitude(new Mapbox.Utils.Vector2d(location.Latitude, location.Longitude));
+            // Map.SetZoom(18);
+            Map.UpdateMap();
+        }
+
+        void OnEnable()
+        {
+            Debug.Log("Enable!!!!!!!!");
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        void OnDisable()
+        {
+            // ARLocationProvider.Instance.OnEnabled.RemoveListener(onLocationEnabled);
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            Debug.Log($"Scene Loaded: {scene.name}");
+        }
+
+        void drawMap()
+        {
+            var tw = RenderTexture.width;
+            var th = RenderTexture.height;
+
+            var scale = MapSize / th;
+            var newWidth = scale * tw;
+            var x = Screen.width / 2 - newWidth / 2;
+            float border;
+            if (x < 0)
+            {
+                border = -x;
+            }
+            else
+            {
+                border = 0;
+            }
+
+
+            GUI.DrawTexture(new Rect(x, Screen.height - MapSize, newWidth, MapSize), RenderTexture, ScaleMode.ScaleAndCrop);
+            GUI.DrawTexture(new Rect(0, Screen.height - MapSize - 20, Screen.width, 20), separatorTexture, ScaleMode.StretchToFill, false);
+          
+            var newZoom = GUI.HorizontalSlider(new Rect(0, Screen.height - 60, Screen.width, 60), Map.Zoom, 10, 22);
+          
+            if (newZoom != Map.Zoom)
+            {
+                Map.SetZoom(newZoom);
+                Map.UpdateMap();
+                // buildMinimapRoute(currentResponse);
+            }
+        }
+
+      
+
+        private Texture2D _separatorTexture;
+        private Texture2D separatorTexture
+        {
+            get
+            {
+                if (_separatorTexture == null)
+                {
+                    _separatorTexture = new Texture2D(1, 1);
+                    _separatorTexture.SetPixel(0, 0, new Color(0.15f, 0.15f, 0.15f));
+                    _separatorTexture.Apply();
+                }
+
+                return _separatorTexture;
+            }
+        }
+
+        public void StartRoute(Location dest)
+        {
+            s.destination = dest;
+
+            if (ARLocationProvider.Instance.IsEnabled)
+            {
+                loadRoute(ARLocationProvider.Instance.CurrentLocation.ToLocation());
+            }
+            else
+            {
+                ARLocationProvider.Instance.OnEnabled.AddListener(loadRoute);
+            }
+        }
+
+        public void EndRoute()
+        {
+            ARLocationProvider.Instance.OnEnabled.RemoveListener(loadRoute);
+            ARSession.SetActive(false);
+            ARSessionOrigin.SetActive(false);
+            RouteContainer.SetActive(false);
+            Camera.gameObject.SetActive(true);
+            s.View = View.SearchMenu;
+        }
+
+        private void loadRoute(Location _)
+        {
+            if (s.destination != null)
+            {
+                var api = new MapboxApi(MapboxToken);
+                var loader = new RouteLoader(api);
+                StartCoroutine(
+                        loader.LoadRoute(
+                            new RouteWaypoint { Type = RouteWaypointType.UserLocation },
+                            new RouteWaypoint { Type = RouteWaypointType.Location, Location = s.destination },
+                            (err, res) =>
+                            {
+                                if (err != null)
+                                {
+                                    s.ErrorMessage = err;
+                                    s.Results = new List<GeocodingFeature>();
+                                    return;
+                                }
+
+                                ARSession.SetActive(true);
+                                ARSessionOrigin.SetActive(true);
+                                RouteContainer.SetActive(true);
+                                Camera.gameObject.SetActive(false);
+                                s.View = View.Route;
+
+                                currentPathRenderer.enabled = true;
+                                MapboxRoute.RoutePathRenderer = currentPathRenderer;
+                                MapboxRoute.BuildRoute(res);
+                                currentResponse = res;
+                                buildMinimapRoute(res);
+                            }));
+            }
+        }
+
+        private GameObject minimapRouteGo;
+        private RouteResponse currentResponse;
+
+        private void buildMinimapRoute(RouteResponse res)
+        {
+            var geo = res.routes[0].geometry;
+            var vertices = new List<Vector3>();
+            var indices = new List<int>();
+
+            var worldPositions = new List<Vector2>();
+
+            foreach (var p in geo.coordinates)
+            {
+                /* var pos = Mapbox.Unity.Utilities.Conversions.GeoToWorldPosition(
+                        p.Latitude,
+                        p.Longitude,
+                        Map.CenterMercator,
+                        Map.WorldRelativeScale
+                        ); */
+
+                // Mapbox.Unity.Utilities.Conversions.GeoToWorldPosition
+                var pos = Map.GeoToWorldPosition(new Mapbox.Utils.Vector2d(p.Latitude, p.Longitude), true);
+                worldPositions.Add(new Vector2(pos.x, pos.z));
+                // worldPositions.Add(new Vector2((float)pos.x, (float)pos.y));
+            }
+
+            if (minimapRouteGo != null)
+            {
+                minimapRouteGo.Destroy();
+            }
+
+            minimapRouteGo = new GameObject("minimap route game object");
+            minimapRouteGo.layer = MinimapLayer;
+
+            var mesh = minimapRouteGo.AddComponent<MeshFilter>().mesh;
+
+            var lineWidth = BaseLineWidth * Mathf.Pow(2.0f, Map.Zoom - 18);
+            LineBuilder.BuildLineMesh(worldPositions, mesh, lineWidth);
+
+            var meshRenderer = minimapRouteGo.AddComponent<MeshRenderer>();
+            meshRenderer.sharedMaterial = MinimapLineMaterial;
+        }
+
+        IEnumerator search()
+        {
+            var api = new MapboxApi(MapboxToken);
+
+            yield return api.QueryLocal(s.QueryText, true);
+
+            if (api.ErrorMessage != null)
+            {
+                s.ErrorMessage = api.ErrorMessage;
+                s.Results = new List<GeocodingFeature>();
+            }
+            else
+            {
+                s.Results = api.QueryLocalResult.features;
+            }
+        }
+
+        Vector3 lastCameraPos;
+        void Update()
+        {
+            if (s.View == View.Route)
+            {
+                var cameraPos = Camera.main.transform.position;
+
+                var arLocationRootAngle = ARLocationManager.Instance.gameObject.transform.localEulerAngles.y;
+                var cameraAngle = Camera.main.transform.localEulerAngles.y;
+                var mapAngle = cameraAngle - arLocationRootAngle;
+
+                MapboxMapCamera.transform.eulerAngles = new Vector3(90, mapAngle, 0);
+
+                if ((cameraPos - lastCameraPos).magnitude < MinimapStepSize) {
+                    return;
+                }
+
+                lastCameraPos = cameraPos;
+
+                var location = ARLocationManager.Instance.GetLocationForWorldPosition(Camera.main.transform.position);
+
+                Map.SetCenterLatitudeLongitude(new Mapbox.Utils.Vector2d(location.Latitude, location.Longitude));
+                Map.UpdateMap();
+
+            }
+            else
+            {
+                MapboxMapCamera.transform.eulerAngles = new Vector3(90, 0, 0);
+            }
+        }
+    }
 }
